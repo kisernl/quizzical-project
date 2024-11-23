ee4be3c82901938b642ceb95ede3841a9cbaabd2
kisernl <noah.kiser@gmail.com>
2024-11-23 08:57:48 -0600

    create diable as well as check answer logic

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -12,18 +12,45 @@ function insertRandomly(array, correctAnswer) {
 }
 
 const randomAnswers = insertRandomly(props.questions.incorrect_answers, props.questions.correct_answer);
+const [isClicked, setIsClicked] = React.useState(false)
+const [highlightBtn, setHighlightBtn] = React.useState(null);
+const [isDisabled, setIsDisabled] = React.useState(false);
 
-// create onClick that highlights selected answer
-// use state to track and allow only one selected answer for question
+const clickAnswer = (index) => {
+    setHighlightBtn(index)
+    setIsClicked(true)
+}
+
+React.useEffect(() => {
+   if (props.checkAnswer === true) {
+    setIsDisabled(true)
+   }
+}, [props.checkAnswer])
+
+// ✅ create onClick that highlights selected answer
+// ✅ use state to track and allow only one selected answer for question
+// create function that checks highlighted answer with correct answer
+    // all correct answers turn green, but if selected answer is other than correct it turns red
 
     return (
         <section className="question--container">
             <p className="question">{decode(props.questions.question)}</p>
             <div>
-                <button className="answer">{decode(randomAnswers[0])}</button>
-                <button className="answer">{decode(randomAnswers[1])}</button>
-                <button className="answer">{decode(randomAnswers[2])}</button>
-                <button className="answer">{decode(randomAnswers[3])}</button>
+                {randomAnswers.map((answer, index) => (
+                    <button
+                        key={index}
+                        className="answer"
+                        onClick={() => clickAnswer(index)}
+                        style={{
+                            backgroundColor: highlightBtn === index ? "#D6DBF5" : "transparent",
+                            ...(highlightBtn === index && { border: "solid #D6DBF5 .1rem" }),
+                        }}
+                        disabled={isDisabled}
+                    >
+                        {decode(answer)}
+                    </button>
+                ) )}
+                
             </div>
             <hr />
         </section>
@@ -36,3 +63,8 @@ const randomAnswers = insertRandomly(props.questions.incorrect_answers, props.qu
 <button className="answer">{decode(props.questions.incorrect_answers[1])}</button>
 <button className="answer">{decode(props.questions.incorrect_answers[2])}</button>
 <button className="answer">{decode(props.questions.correct_answer)}</button> */}
+
+{/* <button className="answer" onClick={clickAnswer} style={{ backgroundColor: highlightBtn }}>{decode(randomAnswers[0])}</button>
+<button className="answer" onClick={clickAnswer} style={{ backgroundColor: highlightBtn }}>{decode(randomAnswers[1])}</button>
+<button className="answer" onClick={clickAnswer} style={{ backgroundColor: highlightBtn }}>{decode(randomAnswers[2])}</button>
+<button className="answer" onClick={clickAnswer} style={{ backgroundColor: highlightBtn }}>{decode(randomAnswers[3])}</button> */}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,32 +3,41 @@ import Question from "./Question";
 
 export default function Quiz(props) {
 
-// create state for trivia object
-// create useEffect function to fetch 5 Qs from API, runs only once
-// const [questions, setQuestions] = React.useState(null)
+// ✅ create state for trivia object
+// ✅ create useEffect function to fetch 5 Qs from API, runs only once
+// ✅ const [questions, setQuestions] = React.useState(null)
+// create state to trigger checking answers of all questions against correct answer
+// create state to track clicked/selected answers
+// create conditional css to highlight selected answers
+// create logic that allows only one answer to be selected, and selecting another answer deselects previous selection
+// create function to be triggered by check answers button to compare selected answer with correct answer
+// create conditional css that highlights correct answers green and incorrect red
+// create conditional text for with score and play again button
 
 const [questions, setQuestions] = React.useState(null)
+const [checkAnswer, setCheckAnswer] = React.useState(false)
 
 React.useEffect(() => {
-    console.log("effect ran")
+    // console.log("effect ran")
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
     .then(response => response.json())
     .then(data => setQuestions(data.results))
 }, [])
 
+const clickCheck = () => {
+    setCheckAnswer(true)
+}
+
 if (questions !== null) {
-console.log("log from Quiz: ", questions[3]);
-
-
 
     return (
          <section className='quiz--container'> 
-            <Question questions={questions[0]} />
-            <Question questions={questions[1]} />
-            <Question questions={questions[2]} />
-            <Question questions={questions[3]} />
-            <Question questions={questions[4]} />
-            <button className="check--btn">Check Answers</button>
+            <Question questions={questions[0]} checkAnswer={checkAnswer} />
+            <Question questions={questions[1]} checkAnswer={checkAnswer} />
+            <Question questions={questions[2]} checkAnswer={checkAnswer} />
+            <Question questions={questions[3]} checkAnswer={checkAnswer} />
+            <Question questions={questions[4]} checkAnswer={checkAnswer} />
+            <button className="check--btn" onClick={clickCheck}>Check Answers</button>
         </section>
     )
 }
